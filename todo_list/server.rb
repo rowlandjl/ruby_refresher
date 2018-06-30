d568e09567f35e8ac174c997cc9ca6db82670261
rowlandjl <rowlandjl@vcu.edu>
2018-06-30 16:17:20 -0400

    add sinatra app practice

--- todo_list/server.rb
+++ todo_list/server.rb
@@ -2,3 +2,17 @@ require "sinatra"
 
 set :bind, '0.0.0.0'
 set :public_folder, File.join(File.dirname(__FILE__), "public")
+
+get "/hello" do
+  "<p>Hello, world! The current time is #{Time.now}.</p>"
+end
+
+get "/tasks" do
+  @tasks = ["pay the bills", "buy milk", "learn ruby"]
+  erb :index
+end
+
+get "/tasks/:task_name" do
+  @task_name = params[:task_name]
+  erb :show
+end
